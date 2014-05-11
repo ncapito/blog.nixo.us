6ad112a68205ccebb5a60d0dcb2ed94923ffdb27
Nick <ncapito@gmail.com>
2014-05-11 17:59:38 -0400

    cleaning up

--- config.js
+++ config.js
@@ -9,6 +9,23 @@ config = {
     // ### Production
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
+    development: {
+        url: 'http://localhost/',
+        mail: {},
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '3002'
+        }
+    },
     production: {
         url: 'https://nixo.us/',
         mail: {},
