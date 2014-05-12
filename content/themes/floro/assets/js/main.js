a68fbfdb99f2e8e0cb4de6832a123240286b1cbd
Nick <ncapito@gmail.com>
2014-05-11 20:40:34 -0400

    fixing post lists

--- content/themes/floro/assets/js/main.js
+++ content/themes/floro/assets/js/main.js
@@ -125,7 +125,7 @@ $(document).ready(function(){
             alert('Unable to load feed, Incorrect path or invalid feed');
         },
         success: function(xml){
-            var postlist = xml.postlist//xml.responseData.feed.entries;
+            var postlist = xml.post || []; //xml.responseData.feed.entries;
             var html= '';
             var htmlselect= '';
             $.each(postlist, function(idx, data) {
